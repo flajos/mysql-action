fb40eb5b3a5ef3fb09c126daa81b4d50a40ef4d2
Lajos Fazakas <lajos@artkonekt.com>
2019-09-26 11:40:40 +0300

    Change host to IP

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,4 +35,4 @@ docker_run="$docker_run --character-set-server=$INPUT_CHARACTER_SET_SERVER --col
 
 sh -c "$docker_run"
 
-docker exec $INPUT_CONTAINER_NAME mysql -u root --password="$INPUT_MYSQL_PASSWORD" -D $INPUT_MYSQL_DATABASE -h mysql --port=$INPUT_CONTAINER_PORT --protocol=TCP "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
+docker exec $INPUT_CONTAINER_NAME mysql -u root --password="$INPUT_MYSQL_PASSWORD" -D $INPUT_MYSQL_DATABASE -h 127.0.0.1 --port=$INPUT_CONTAINER_PORT --protocol=TCP "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
